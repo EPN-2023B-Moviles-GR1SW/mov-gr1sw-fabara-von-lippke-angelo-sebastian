09786cd4dc4d75a2a562626aad8fe5823608e979
Angelo Fabara <angelo.fabara@epn.edu.ec>
2023-12-13 14:49:06 -0500

    clase 10

---
+++ 02 - Android/app/src/main/java/com/example/b2023_gr1sw_asfv/BEntrenador.kt
@@ -0,0 +1,8 @@
+package com.example.b2023_gr1sw_asfv
+
+class BEntrenador(
+    var id: Int,
+    var nombre: String?,
+    var descripcion: String?
+) {
+}
--- 02 - Android/app/src/main/java/com/example/b2023_gr1sw_asfv/MainActivity.kt
+++ 02 - Android/app/src/main/java/com/example/b2023_gr1sw_asfv/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.b2023_gr1sw_asfv
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -16,5 +18,21 @@ import com.example.b2023_gr1sw_asfv.ui.theme.B2023gr1swasfvTheme
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+        botonCicloVida
+            .setOnClickListener{
+                irActividad(ACicloVida::class.java)
+            }
+        val botonListView = findViewById<Button>(R.id.btn_ir_list_view)
+        botonListView
+            .setOnClickListener {
+                irActividad(BListView::class.java)
+            }
+    }
+    fun irActividad(clase: Class<*>){
+        val intent = Intent(this,clase)
+        startActivity(intent)
     }
 }

359bf83e399fb8e6f38e86e252a7a86a4e28f1fe
Angelo Fabara <angelo.fabara@epn.edu.ec>
2024-01-24 15:59:13 -0500

    clase 20

--- 02 - Android/app/src/main/java/com/example/b2023_gr1sw_asfv/MainActivity.kt
+++ 02 - Android/app/src/main/java/com/example/b2023_gr1sw_asfv/MainActivity.kt
@@ -72,6 +72,13 @@ class MainActivity : AppCompatActivity() {
             .setOnClickListener {
                 irActividad(GGoogleMapsActivity::class.java)
             }
+        val botonFirebaseUI = findViewById<Button>(
+            R.id.btn_intent_firebase_ui
+        )
+        botonFirebaseUI
+            .setOnClickListener {
+                irActividad(HFirebaseUIAuth::class.java)
+            }
     }
 
     private fun abrirActividadConParametros(clase: Class<*>) {

e28f0950250ccbebacba8ae25f88ed8f3c5fbacc
Angelo Fabara <angelo.fabara@epn.edu.ec>
2024-03-05 22:35:02 -0500

    Proyecto2B

---
+++ 05 - Proyectos/ProyectoIIB/app/src/main/java/com/kulnois/notesapp/adapter/ItemNoteBinding.kt
@@ -0,0 +1,17 @@
+package com.kulnois.notesapp.adapter
+
+import android.view.View
+import com.kulnois.notesapp.model.Note
+
+class ItemNoteBinding(val root: View, var note: Note) {
+    fun executePendingBindings() {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        fun inflate(inflater: Any): ItemNoteBinding {
+
+        }
+    }
+
+}
